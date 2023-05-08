4604eb55b92103d73be356478a56cee98150dc95
stm-robotics <124404090+stm-robotics@users.noreply.github.com>
2023-05-07 20:19:45 -0500

    Session 5/6 Updates

--- Addison.h
+++ Addison.h
@@ -10,12 +10,15 @@ void turnRight(int RIGHT_MOTOR_PORT, int LEFT_MOTOR_PORT)
 	//move_to_position(RIGHT_MOTOR_PORT,-500,-1500/4);
     //move_to_position(LEFT_MOTOR_PORT,500,1500/4);
     //msleep(3000);
-    //mrp(LEFT_MOTOR_PORT,-1000,3000); //changed 5/1/23 from 3000 to 2556 //this should be mav
+    mrp(LEFT_MOTOR_PORT,-1000,3000); //changed 5/1/23 from 3000 to 2556 //this should be mav
     //mav(RIGHT_MOTOR_PORT,-700);
     //mav(LEFT_MOTOR_PORT,900);
-    cmpc(RIGHT_MOTOR_PORT);
-    cmpc(LEFT_MOTOR_PORT);
-    mtp(LEFT_MOTOR_PORT,1000, 2500);
+    
+    //cmpc(RIGHT_MOTOR_PORT);
+    //cmpc(LEFT_MOTOR_PORT);
+ /****** suggest to tweak the click value to something slightly less *******/
+    //mtp(LEFT_MOTOR_PORT,1000, 3000);
+    
     msleep(4000);
 }
 
--- Violet.h
+++ Violet.h
@@ -16,13 +16,13 @@ void DebugPrint(char message[])
     }
 }
 
-
-
 void followWall(int ET_PORT, int LEFTWHEEL_PORT, int RIGHTWHEEL_PORT)
 {
-    int iMaxTime = 7000;
+    //Use DebugPrint to output the start of the function
+    DebugPrint("Violet: START followWall\n");
+    int iMaxTime = 6000;
     int iCurrentTime = 0;
-    int bDriveRight = 0;
+    //int bDriveRight = 0;
     int iLeftWheelSpeed;
     int iRightWheelSpeed;
     while(iCurrentTime < iMaxTime){     
@@ -30,8 +30,8 @@ void followWall(int ET_PORT, int LEFTWHEEL_PORT, int RIGHTWHEEL_PORT)
         if (analog(ET_PORT) >= ET_MIN_RANGE && analog(ET_PORT) <= ET_MAX_RANGE) {
             while (iCurrentTime < iMaxTime && analog(ET_PORT) >= ET_MIN_RANGE && analog(ET_PORT) <= ET_MAX_RANGE) {
             	printf("drive straight - %d\n", iCurrentTime);
-        		iLeftWheelSpeed = 1500;
-            	iRightWheelSpeed = 1500;
+        		iLeftWheelSpeed = 1000;
+            	iRightWheelSpeed = 1000;
             	mav(LEFTWHEEL_PORT,iLeftWheelSpeed);
             	mav(RIGHTWHEEL_PORT,iRightWheelSpeed);
            	 	msleep(1);
@@ -42,8 +42,8 @@ void followWall(int ET_PORT, int LEFTWHEEL_PORT, int RIGHTWHEEL_PORT)
             while (iCurrentTime < iMaxTime && analog(ET_PORT) > ET_MAX_RANGE) {
             	printf("drive left - %d\n", iCurrentTime);
                 printf("ET SENSOR = %i\n", analog(ET_PORT));
-        		iLeftWheelSpeed = 200;
-            	iRightWheelSpeed = 300;
+        		iLeftWheelSpeed = 800;
+            	iRightWheelSpeed = 900;
             	mav(LEFTWHEEL_PORT,iLeftWheelSpeed);
             	mav(RIGHTWHEEL_PORT,iRightWheelSpeed);
                 msleep(1);
@@ -55,8 +55,8 @@ void followWall(int ET_PORT, int LEFTWHEEL_PORT, int RIGHTWHEEL_PORT)
             while (iCurrentTime < iMaxTime && analog(ET_PORT) < ET_MAX_RANGE) {
             	printf("gentle drive right - %d\n", iCurrentTime);
                 printf("ET SENSOR = %i\n", analog(ET_PORT));
-        		iLeftWheelSpeed = 1500;
-            	iRightWheelSpeed = 1350;
+        		iLeftWheelSpeed = 900;
+            	iRightWheelSpeed = 800;
             	mav(LEFTWHEEL_PORT,iLeftWheelSpeed);
             	mav(RIGHTWHEEL_PORT,iRightWheelSpeed);
                 msleep(1);
@@ -70,9 +70,9 @@ void followWall(int ET_PORT, int LEFTWHEEL_PORT, int RIGHTWHEEL_PORT)
 
 void followWallBack(int ET_PORT, int LEFTWHEEL_PORT, int RIGHTWHEEL_PORT, int LEFTBUTTON_PORT, int RIGHTBUTTON_PORT)
 {
-    int iMaxTime = 16000;
+    //int iMaxTime = 16000;
     int iCurrentTime = 0;
-    int bDriveRight = 0;
+    //int bDriveRight = 0;
     int iLeftWheelSpeed;
     int iRightWheelSpeed;
     while(!(digital(LEFTBUTTON_PORT) || digital(RIGHTBUTTON_PORT))){     
@@ -88,7 +88,7 @@ void followWallBack(int ET_PORT, int LEFTWHEEL_PORT, int RIGHTWHEEL_PORT, int LE
             	iRightWheelSpeed = -1500;
             	mav(LEFTWHEEL_PORT,iLeftWheelSpeed);
             	mav(RIGHTWHEEL_PORT,iRightWheelSpeed);
-           	 	msleep(100);
+           	 	msleep(1);
                 iCurrentTime += 3;
             }
         }
@@ -100,15 +100,13 @@ void followWallBack(int ET_PORT, int LEFTWHEEL_PORT, int RIGHTWHEEL_PORT, int LE
             	iRightWheelSpeed = -300;
             	mav(LEFTWHEEL_PORT,iLeftWheelSpeed);
             	mav(RIGHTWHEEL_PORT,iRightWheelSpeed);
-                msleep(100);
+                msleep(1);
                 iCurrentTime += 3;
             }
             
         }
-        //changed 5/1
         else if (analog(ET_PORT) < ET_MIN_RANGE){ 
-           //while (!(digital(LEFTBUTTON_PORT) || digital(RIGHTBUTTON_PORT)) && analog(ET_PORT) < ET_MAX_RANGE) {
-            while (!(digital(LEFTBUTTON_PORT) || digital(RIGHTBUTTON_PORT)) && analog(ET_PORT) < ET_MIN_RANGE) {
+            while (!(digital(LEFTBUTTON_PORT) || digital(RIGHTBUTTON_PORT)) && analog(ET_PORT) < ET_MAX_RANGE) {
             	printf("gentle drive right - %d\n", iCurrentTime);
                 printf("ET SENSOR = %i\n", analog(ET_PORT));
         		iLeftWheelSpeed = -1500;
